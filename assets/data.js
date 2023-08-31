fd8669309233f146d3135b44e56ce2cd5e54d45c
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-09-01 01:41:22 +0530

    feat - implementation of icons while right and left swipe done

--- assets/data.js
+++ assets/data.js
@@ -1,27 +1,22 @@
 export const data = [
   {
     id: 1,
-    name: "first",
     color: "#7DF9FF",
   },
   {
     id: 2,
-    name: "second",
     color: "#00FF7F",
   },
   {
     id: 3,
-    name: "third",
     color: "#FFDB58",
   },
   {
     id: 4,
-    name: "fourth",
     color: "#F28C28",
   },
   {
     id: 5,
-    name: "fifth",
     color: "#FF3131",
   },
 ];
